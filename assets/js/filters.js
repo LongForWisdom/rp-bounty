ae0bebe4941e1fdf174dc1b447a0824c762eb61c
LongForWisdom <LongForWisdom@pm.me>
2023-12-04 17:36:43 +0000

    Added dynamic sort

--- assets/js/filters.js
+++ assets/js/filters.js
@@ -1,42 +1,99 @@
 <script>
 var bounties = {{bounties | jsonify}};
+var bountyLookup = {};
+var sorts = {
+  "date-desc": { displayName: "Newest", comparator: function(a,b) { return Date.parse(bountyLookup[b.id].date) - Date.parse(bountyLookup[a.id].date);}},
+  "date-asc": { displayName: "Oldest", comparator: function(a,b) { return sorts["date-desc"].comparator(b,a);}},
+};
 
 
 document.addEventListener("DOMContentLoaded", function() {
-  loadFilters();
-  filterBounties(false);
+  bounties.forEach((bounty) => { bountyLookup[bounty.code] = bounty; });
+  addSortButtons();
+  
+  loadParams();
+  saveParams(false);
 });
 
 addEventListener("popstate", function() {
   location.reload();
-  loadFilters();
-  filterBounties(false);
+  loadParams();
+  saveParams(false);
 });
 
+function toggleClass(element, cssClass)
+{
+  document.getElementById(element).classList.toggle(cssClass);
+}
 
-function loadFilters() {
+function loadParams() {
   
   const params = (new URL(document.location)).searchParams;
+  const filters = ["skillset", "status" ,"tag"];
+  let hasFilterString = false;
   
-    console.log("load");
-    console.log(params);
-    for (const [key, value] of params) {
-    
+  for (const [key, value] of params) {
     let values = value.split('+');
-    console.log(values);
     values.forEach(function(value)
     {
-      let elementId = "inlineCheckbox-" + key + ":" + value;
-      console.log(elementId);
-      let element = document.getElementById(elementId);
-      element.checked = true;
+      if(filters.includes(key))
+      {
+        let elementId = "inlineCheckbox-" + key + ":" + value;
+        let element = document.getElementById(elementId);
+        element.checked = true;
+        hasFilterString = true;
+      }
+      else if(key == "sort")
+      {
+        let sortDisplay = document.getElementById("sort-button");
+        sortDisplay.dataset.sort = value;
+      }
     });
   }
   
+  if(hasFilterString) filterBounties(false);  
+  sortBounties();
+}
+
+function saveParams(shouldPushState = true) {
+  // Get searchParams
+  const prevParams = (new URL(document.location)).searchParams;
+  let paramsObject = {};
+  Object.assign(paramsObject, prevParams.searchParams);
+  
+  
+  // get filter values
+  let selectedFilters = Array.from(document.querySelectorAll('input[name=filter]:checked'));
+  if(selectedFilters.length > 0)
+  {
+    let filterObj = selectedFilters.reduce(function(acc, curr){
+      let pair = curr.value.split(':');
+      let value = acc[pair[0]] === undefined ? [] : acc[pair[0]];
+      value.push(pair[1]);
+      acc[pair[0]] = value;
+      return acc;
+    }, {});
+    
+    if(filterObj.status !== undefined && filterObj.status.length > 0) paramsObject.status = filterObj.status.join('+');
+    if(filterObj.skillset !== undefined && filterObj.skillset.length > 0) paramsObject.skillset = filterObj.skillset.join('+');
+    if(filterObj.tag !== undefined && filterObj.tag.length > 0) paramsObject.tag = filterObj.tag.join('+');
+  }
+  
+  //get sort value
+  let sortDisplay = document.getElementById("sort-button");
+  let sortType = sortDisplay.dataset.sort;
+  
+  paramsObject.sort = sortType;
+  
+  const newParams = new URLSearchParams(paramsObject);
+  if((newParams != prevParams) && shouldPushState === true)
+  {
+    history.pushState("", "", '?' + newParams);
+  }
 }
 
 // filter resources to only ones with all checked categories
-function filterBounties(shouldPushState = true) {
+function filterBounties() {
   // get a list of checked filters
   let allFilters = Array.from(document.querySelectorAll('input[name=filter]'));
   let selectedFilters = Array.from(document.querySelectorAll('input[name=filter]:checked'));
@@ -44,7 +101,6 @@ function filterBounties(shouldPushState = true) {
   let selectedStatuses = [];
   let selectedSkillsets = [];
   let selectedTags = [];  
-  let paramsObject = {};
   
   selectedFilters.forEach(function(input)
   {
@@ -63,73 +119,89 @@ function filterBounties(shouldPushState = true) {
       break;
     }
   });
-  
+
   let searchInput = searchFilter.value.toLowerCase();
-  
-  if(selectedStatuses.length > 0) paramsObject.status = selectedStatuses.join('+');
-  if(selectedSkillsets.length > 0) paramsObject.skillset = selectedSkillsets.join('+');
-  if(selectedTags.length > 0) paramsObject.tag = selectedTags.join('+');
 
-  const prevParams = (new URL(document.location)).searchParams;
-  const newParams = new URLSearchParams(paramsObject);
-  if((newParams != prevParams) && shouldPushState === true)
-  {
-    console.log("push");
-    history.pushState("", "", '?' + newParams);
-  }
-
-    var items = document.getElementsByClassName("bounty-item-container");
-    for (let i = 0; i < items.length; i++) {
-      let show = true;
-      let bounty = bounties.find((bounty) => bounty.code === items[i].id);
-      
-      show &= selectedStatuses.includes(bounty.status) || selectedStatuses.length === 0;
-      
-      if(selectedSkillsets.length > 0)
+  let items = document.getElementsByClassName("bounty-item-container");
+  for (let i = 0; i < items.length; i++) {
+    let show = true;
+    let bounty = bounties.find((bounty) => bounty.code === items[i].id);
+    
+    show &= selectedStatuses.includes(bounty.status) || selectedStatuses.length === 0;
+    
+    if(selectedSkillsets.length > 0)
+    {
+      let skillsetShow = false;
+      bounty.skillsets.forEach((bountySkillset) => skillsetShow |= selectedSkillsets.includes(bountySkillset));
+      show &= skillsetShow;
+    }
+    
+    if(selectedTags.length > 0)
+    {
+      if(bounty.tags !== null && bounty.tags.length !== 0)
       {
-        let skillsetShow = false;
-        bounty.skillsets.forEach((bountySkillset) => skillsetShow |= selectedSkillsets.includes(bountySkillset));
-        show &= skillsetShow;
-      }
-      
-      if(selectedTags.length > 0)
-      {
-        if(bounty.tags !== null && bounty.tags.length !== 0)
-        {
-          let tagShow = false;
-          bounty.tags.forEach((bountyTag) => tagShow |= selectedTags.includes(bountyTag));
-          show &= tagShow;
-        }
-        else
-        {
-          show = false;
-        }
-      }
-      
-      if(searchInput !== null && searchInput !== "")
-      {
-        let lowercaseTitle = bounty.title.toLowerCase();
-        let lowercaseSummary = bounty.summary.toLowerCase();
-        if(lowercaseTitle.includes(searchInput) ||
-           lowercaseSummary.includes(searchInput))
-        {
-          show &= true;
-        }
-        else
-        {
-          show = false;
-        }
-      }
-        
-      
-      if(show)
-      {
-        items[i].classList.remove("bounty-item-anim-hide");
+        let tagShow = false;
+        bounty.tags.forEach((bountyTag) => tagShow |= selectedTags.includes(bountyTag));
+        show &= tagShow;
       }
       else
       {
-        items[i].classList.add("bounty-item-anim-hide");
+        show = false;
       }
     }
+    
+    if(searchInput !== null && searchInput !== "")
+    {
+      let lowercaseTitle = bounty.title.toLowerCase();
+      let lowercaseSummary = bounty.summary.toLowerCase();
+      if(lowercaseTitle.includes(searchInput) ||
+         lowercaseSummary.includes(searchInput))
+      {
+        show &= true;
+      }
+      else
+      {
+        show = false;
+      }
+    }
+      
+    
+    if(show)
+    {
+      items[i].classList.remove("bounty-item-anim-hide");
+    }
+    else
+    {
+      items[i].classList.add("bounty-item-anim-hide");
+    }
+  }
 }
+
+function addSortButtons()
+{
+  let sortDisplay = document.getElementById("sort-button");
+  let sortDropdown = document.getElementById("sort-dropdown");
+  Object.keys(sorts).forEach( key => {
+    let sortButton = document.createElement("button");
+    sortButton.onclick = function(){ sortDisplay.dataset.sort = key; sortBounties(); saveParams(); sortDisplay.click();};
+    sortButton.dataset.sort = key;
+    sortButton.textContent = sorts[key].displayName;
+    sortButton.classList.add("sort-dropdown-button");
+    sortDropdown.appendChild(sortButton);
+  });
+}
+
+function sortBounties()
+{
+  let elements = document.getElementsByClassName("bounty-item-container");
+  let parent = document.getElementById("bounty-list");
+  let sortDisplay = document.getElementById("sort-button");
+  let elementArray = Array.from(elements);
+  let sortType = sortDisplay.dataset.sort;
+
+  let sorted = elementArray.sort(sorts[sortType].comparator);
+  sorted.forEach((item) => { parent.appendChild(item);});
+  sortDisplay.textContent = "Sort By: " + sorts[sortType].displayName;
+}
+
 </script>
